0d2118fc35021b5af8fe9eb5e08b5e705aed18b1
Foxbat323 <coknindya67@gmail.com>
2026-05-22 14:58:01 +0800

    Complete Subscription Module

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\SubscriptionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('services', ServiceController::class);
 Route::apiResource('customers', CustomerController::class);
+Route::apiResource('subscriptions', SubscriptionController::class);
